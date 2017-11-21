431d4bc8cc479d3b3f7c036e8107419dd2c408a9
Adam Gallinat <agallinat@kqed.org>
2017-11-21 10:49:23 -0800

    Add + Delete buttons, fix product2 assignment

--- force-app/main/default/aura/ProductPickerLineItem/ProductPickerLineItemController.js
+++ force-app/main/default/aura/ProductPickerLineItem/ProductPickerLineItemController.js
@@ -1,8 +1,23 @@
 ({
     setSelectedProduct : function(component, event, helper) {
-        const selectedProduct = component.find('ProductSelection').get('v.value');
-        const theOli = component.get('v.oli');
-        theOli.Product2Id = selectedProduct;
-        component.set('v.oli', theOli);
+        const selectedProductId = component.find('ProductSelection').get('v.value');
+        const pbes = component.get('v.pbes');
+        let selectedProduct;
+        for (let i = 0; i < pbes.length; i++) {
+            if (pbes[i].Product2Id === selectedProductId) {
+                selectedProduct = pbes[i];
+                break;
+            }
+        }
+        const theOli = component.get('v.wrapper');
+        theOli.oli.Product2Id = selectedProductId;
+        theOli.oli.UnitPrice = selectedProduct.UnitPrice;
+        component.set('v.wrapper', theOli);
+    },
+
+    stageForDeletion : function(component, event, helper) {
+        const theOli = component.get('v.wrapper');
+        theOli.deleteFlag = true;
+        component.set('v.wrapper', theOli);
     }
 })
